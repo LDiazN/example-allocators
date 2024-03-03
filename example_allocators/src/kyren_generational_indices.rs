7ee3d3a6da95d5c22851446b856a76f84d67546f
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2024-03-03 19:39:36 -0400

    Removed useless trait file; Improved example with storage in the basic kyren implementation

--- example_allocators/src/kyren_generational_indices.rs
+++ example_allocators/src/kyren_generational_indices.rs
@@ -2,6 +2,7 @@
 /// presented here: https://kyren.github.io/2018/09/14/rustconf-talk.html
 /// 
 /// This is the base implementation I will be testing my allocators with.
+use std::collections::VecDeque;
 
 #[derive(Debug, PartialEq, Default)]
 /// This is the simplest implementation, this struct will tell you which index
@@ -9,7 +10,7 @@
 pub struct GenerationalIndices
 {
     indices : Vec<u32>, // Generations. Indices are specified by the array position
-    pub free : Vec<usize>
+    pub free : VecDeque<usize>
 }
 
 #[derive(Debug, PartialEq, Default, Clone)]
@@ -21,13 +22,7 @@ pub struct GenerationalIndex
 
 impl GenerationalIndices
 {
-    #[inline(always)]
-    pub fn new() -> Self
-    {
-        GenerationalIndices { indices: vec![], free: vec![] }
-    }
-
-    pub fn get(&mut self) -> GenerationalIndex
+    pub fn new(&mut self) -> GenerationalIndex
     {
         if self.free.is_empty()
         {
@@ -37,7 +32,7 @@ impl GenerationalIndices
             return GenerationalIndex{index: next_index, generation: 0};
         }
 
-        let index = self.free.pop().unwrap();
+        let index = self.free.pop_front().unwrap();
         let generation = self.indices[index];
 
         GenerationalIndex { index, generation}
@@ -56,7 +51,7 @@ impl GenerationalIndices
             return; // Report an error or something
         }
 
-        self.free.push(index.index);
+        self.free.push_back(index.index);
         self.indices[index.index] += 1;
     }
 }
@@ -77,16 +72,10 @@ impl GenerationalIndex
 }
 
 // -- < Versions with the actual storage > ------------------------
-pub trait LifeCycle {
-    fn destroy(&mut self);
-
-    fn set_index(&mut self, index: GenerationalIndex);
-}
+#[derive(Debug, Default)]
 pub struct GenerationalArrayEntry<T>
 {
-    // Kyren would set this to Option<T> but if we do so we can't get mutable references
-    // to the internal data, only clones
-    item : T, 
+    item : Option<T>,
     generation : u32
 }
 
@@ -97,28 +86,21 @@ pub struct GenerationalArrayEntry<T>
 ///  * You might have to resize an array with too many elements with possibly large storage
 ///  * You have to construct objects in the stack and then copy the entire content into the internal array
 ///  * You might end up with a lot of unused unrecoverable space after a lot of allocations
+#[derive(Debug, Default)]
 pub struct GenerationalIndexArray<T>
-    where T : LifeCycle
 {
     elements : Vec<GenerationalArrayEntry<T>>,
     free: Vec<usize>
 }
 
 impl<T> GenerationalIndexArray<T>
-    where T : LifeCycle
 {
-    #[inline(always)]
-    pub fn new() -> Self
-    {
-        GenerationalIndexArray { elements: vec![], free: vec![] }
-    }
-
-    pub fn store(&mut self, element : T) -> GenerationalIndex
+    pub fn new(&mut self, element : T) -> GenerationalIndex
     {
         if self.free.is_empty()
         {
             let next_index = self.elements.len();
-            let entry = GenerationalArrayEntry{generation: 0, item: element};
+            let entry = GenerationalArrayEntry{generation: 0, item: Some(element)};
             self.elements.push(entry);
 
             return GenerationalIndex{index: next_index, generation: 0};
@@ -126,7 +108,7 @@ impl<T> GenerationalIndexArray<T>
 
         let index = self.free.pop().unwrap();
         let entry = &mut self.elements[index];
-        entry.item = element;
+        entry.item = Some(element);
 
         GenerationalIndex {index, generation: entry.generation}
     }
@@ -146,7 +128,7 @@ impl<T> GenerationalIndexArray<T>
 
         self.free.push(index.index);
         self.elements[index.index].generation += 1;
-        self.elements[index.index].item.destroy();
+        self.elements[index.index].item = None;
     }
 
     pub fn get(&self, index: &GenerationalIndex) -> Option<&T>
@@ -156,7 +138,7 @@ impl<T> GenerationalIndexArray<T>
             return None;
         }
 
-        return Some(&self.elements[index.get_index()].item);
+        return self.elements[index.get_index()].item.as_ref();
     }
 
     pub fn get_mut(&mut self, index: &GenerationalIndex) -> Option<&mut T>
@@ -166,10 +148,9 @@ impl<T> GenerationalIndexArray<T>
             return None;
         }
 
-        return Some(&mut self.elements[index.get_index()].item);
+        return self.elements[index.get_index()].item.as_mut();
     }
 }
 
 // Te previous implementation has some problems about references and pointers. So instead 
 // we will store pointers instead of the entire thing we are allocating.
-
--- example_allocators/src/tests.rs
+++ example_allocators/src/tests.rs
@@ -4,14 +4,14 @@ mod tests
 {
     mod kyren_tests
     {
-        use crate::kyren_generational_indices::{self as kyren, GenerationalIndex, LifeCycle, GenerationalIndexArray};
-    
+        use crate::kyren_generational_indices::{self as kyren, GenerationalIndex, GenerationalIndexArray};
+
         #[test]
         fn test_kyren_get()
         {
-            let mut generational_indices = kyren::GenerationalIndices::new();
+            let mut generational_indices = kyren::GenerationalIndices::default();
     
-            let new_index = generational_indices.get();
+            let new_index = generational_indices.new();
             assert_eq!(new_index.get_index(), 0, "First index should start at 0"); 
             assert_eq!(new_index.get_generation(), 0, "First index should start at 0"); 
         }
@@ -19,15 +19,15 @@ mod tests
         #[test]
         fn test_kyren_successive_gets()
         {
-            let mut generational_indices = kyren::GenerationalIndices::new();
+            let mut generational_indices = kyren::GenerationalIndices::default();
     
-            let index_1 = generational_indices.get();
-            let index_2 = generational_indices.get();
+            let index_1 = generational_indices.new();
+            let index_2 = generational_indices.new();
     
             assert!(index_1.get_index() != index_2.get_index());
     
             generational_indices.free(&index_1); // Index 1 should not be available now
-            let index_1_new = generational_indices.get();
+            let index_1_new = generational_indices.new();
     
             assert_eq!(index_1_new.get_index(), index_1.get_index());
             assert_ne!(index_1_new.get_generation(), index_1.get_generation());
@@ -36,45 +36,35 @@ mod tests
         #[test]
         fn test_kyren_is_alive()
         {
-            let mut generational_indices = kyren::GenerationalIndices::new();
+            let mut generational_indices = kyren::GenerationalIndices::default();
     
-            let index = generational_indices.get();
+            let index = generational_indices.new();
             assert!(generational_indices.is_live(&index));
             generational_indices.free(&index);
             assert!( !generational_indices.is_live(&index) );
         }
     
         /// Dummy implementation of an entity for testing purposes
+        #[derive(Debug, Default)]
         struct Entity
         {
-            id : GenerationalIndex,
+            _id : GenerationalIndex,
             name : String,
-            is_active : bool
-        }
-    
-        impl LifeCycle for Entity
-        {
-            fn destroy(&mut self) {
-                self.is_active = false;
-            }
-    
-            fn set_index(&mut self, index: GenerationalIndex) {
-                self.id = index;
-            }
+            _is_active : bool
         }
     
         #[test]
         fn test_kyren_get_array()
         {
-            let mut generational_array = GenerationalIndexArray::<Entity>::new();
+            let mut generational_array = GenerationalIndexArray::<Entity>::default();
             
             let entity = Entity{
                 name: "Entity1".to_string(),
-                is_active: true,
-                id: GenerationalIndex::default()
+                _is_active: true,
+                _id: GenerationalIndex::default()
             };
     
-            let index = generational_array.store(entity);
+            let index = generational_array.new(entity);
     
             let entity_ref = generational_array.get(&index);
             assert!(entity_ref.is_some());
@@ -85,14 +75,14 @@ mod tests
         #[test]
         fn test_kyren_free_array()
         {
-            let mut generational_array = GenerationalIndexArray::<Entity>::new();
+            let mut generational_array = GenerationalIndexArray::<Entity>::default();
             let entity1 = Entity{
                 name: "Entity1".to_string(),
-                is_active: true,
-                id: GenerationalIndex::default()
+                _is_active: true,
+                _id: GenerationalIndex::default()
             };
     
-            let index = generational_array.store(entity1);
+            let index = generational_array.new(entity1);
             let entity_ref = generational_array.get(&index);
             assert!(entity_ref.is_some());
     
@@ -104,23 +94,23 @@ mod tests
         #[test]
         fn test_kyren_replacement_array()
         {
-            let mut generational_array = GenerationalIndexArray::<Entity>::new();
+            let mut generational_array = GenerationalIndexArray::<Entity>::default();
             let entity1 = Entity{
                 name: "Entity1".to_string(),
-                is_active: true,
-                id: GenerationalIndex::default()
+                _is_active: true,
+                _id: GenerationalIndex::default()
             };
     
             let entity2 = Entity{
                 name: "Entity2".to_string(),
-                is_active: true,
-                id: GenerationalIndex::default()
+                _is_active: true,
+                _id: GenerationalIndex::default()
             };
     
-            let index = generational_array.store(entity1);
+            let index = generational_array.new(entity1);
             generational_array.free(&index);
     
-            let index = generational_array.store(entity2);
+            let index = generational_array.new(entity2);
             let entity_ref = generational_array.get(&index);
     
             assert!(entity_ref.is_some());
